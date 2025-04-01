6c718a143d69f68df7451cab203c12f5dc05847f
Ruslan Salmanov <flainof@gmail.com>
2025-04-01 21:02:18 +0300

    feat: improved mailer

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,22 +1,17 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
-import { getOtpTemplate } from './templates';
 import { OtpType } from '../otp/types';
 import { titles } from './constants';
+import { getOtpTemplate } from './templates';
 
 @Injectable()
 export class MailService {
     constructor(private readonly mailerService: MailerService) {}
 
-    sendOtpEmail = ({ otp, type, email }: { otp: number; type: OtpType; email: string }) => {
-        return this.mailerService.sendMail({
-            from: {
-                name: 'FCHAT',
-                address: process.env.MAILER_USER,
-            },
-            to: email,
-            subject: 'Verification code',
-            html: getOtpTemplate(otp, titles[type]),
-        });
-    };
+    sendOtpEmail = ({ otp, type, email }: { otp: number; type: OtpType; email: string }) => this.mailerService.sendMail({
+        from: 'Syncmate',
+        to: email,
+        subject: 'Verification code',
+        html: getOtpTemplate(otp, titles[type])
+    });
 }
