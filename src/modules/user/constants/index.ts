73a6b7bc51525da51b2ca170eb8caca8a4f28393
Ruslan Salmanov <flainof@gmail.com>
2025-04-03 14:20:04 +0300

    feat: use regexp direct in filetype

--- src/modules/user/constants/index.ts
+++ src/modules/user/constants/index.ts
@@ -1,18 +1,17 @@
-import { z } from 'zod';
 import { FileTypeValidator, MaxFileSizeValidator, ParseFilePipe } from '@nestjs/common';
 import { emailExistError, loginExistError } from 'src/modules/auth/constants';
 import { IAppException } from 'src/utils/types';
+import { z } from 'zod';
 import { userCheckSchema } from '../schemas/user.check.schema';
 
 export const MAX_IMAGE_SIZE = 5 * 1024 ** 2;
 export const validImageTypes = ['image/png', 'image/jpeg', 'image/jpg'];
-export const imageTypeRegex = new RegExp(`^(${validImageTypes.join('|')})$`);
 
 export const filePipe = new ParseFilePipe({
     fileIsRequired: true,
     validators: [
         new MaxFileSizeValidator({ maxSize: MAX_IMAGE_SIZE, message: 'Max file size is 5MB' }),
-        new FileTypeValidator({ fileType: imageTypeRegex }),
+        new FileTypeValidator({ fileType: new RegExp(`^(${validImageTypes.join('|')})$`) }),
     ],
 });
 
