362b02c23dc9eb2e102e9747ce763791803f0cd0
Ruslan Salmanov <flainof@gmail.com>
2025-06-20 13:24:32 +0300

    refactor(user): remove unused imports and streamline image type validation

--- src/modules/user/constants/index.ts
+++ src/modules/user/constants/index.ts
@@ -1,21 +1,21 @@
 import { FileTypeValidator, MaxFileSizeValidator, ParseFilePipe } from '@nestjs/common';
-import { emailExistError, loginExistError } from 'src/modules/auth/constants';
-import { IAppException } from 'src/utils/types';
-import { z } from 'zod';
-import { userCheckSchema } from '../schemas/user.check.schema';
 
 export const MAX_IMAGE_SIZE = 5 * 1024 ** 2;
-export const validImageTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+
+export const privacySettingPaths = [
+    'whoCanAddMeToGroupChats',
+    'whoCanLinkMyProfileViaForward',
+    'whoCanSeeMyBio',
+    'whoCanSeeMyEmail',
+    'whoCanSeeMyLastSeenTime',
+    'whoCanSeeMyProfilePhotos',
+    'whoCanSendMeMessages'
+] as const;
 
 export const filePipe = new ParseFilePipe({
     fileIsRequired: true,
     validators: [
         new MaxFileSizeValidator({ maxSize: MAX_IMAGE_SIZE, message: 'Max file size is 5MB' }),
-        new FileTypeValidator({ fileType: new RegExp(`^(${validImageTypes.join('|')})$`) }),
+        new FileTypeValidator({ fileType: new RegExp(`^(${['image/png', 'image/jpeg', 'image/jpg'].join('|')})$`) }),
     ],
 });
-
-export const checkErrors: Record<z.infer<typeof userCheckSchema>['type'], Pick<IAppException, 'message' | 'errors'>> = {
-    email: emailExistError,
-    login: loginExistError,
-};
