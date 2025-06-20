8c7202356f2172c6ff3921b0bdcf7d452773e163
Ruslan Salmanov <flainof@gmail.com>
2025-06-20 13:23:50 +0300

    feat(otp): add Public decorator to create OTP endpoint for open access

--- src/modules/otp/otp.controller.ts
+++ src/modules/otp/otp.controller.ts
@@ -3,11 +3,13 @@ import { Routes } from 'src/utils/types';
 import { OtpCreateDTO } from './dtos/otp.create.dto';
 import { OtpService } from './otp.service';
 import { OtpVerifyDTO } from './dtos/otp.verify.dto';
+import { Public } from 'src/utils/decorators/public.decorator';
 
 @Controller(Routes.OTP)
 export class OtpController {
     constructor(private readonly otpService: OtpService) {}
 
+    @Public()
     @Post()
     create(@Body() { email, type }: OtpCreateDTO) {
         return this.otpService.createOtp({ email, type });
